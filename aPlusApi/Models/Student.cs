b360a911733d26b4b450e78e2dd55596f2b34937
André Pacini <andrepacini1@hotmail.com>
2025-02-11 20:47:19 -0300

    [back] adicionado coluna Id como auto increment para Ra ser chave unica

--- aPlusApi/Models/Student.cs
+++ aPlusApi/Models/Student.cs
@@ -6,6 +6,7 @@ namespace aPlusApi.Models;
 public class Student
 {
     [Key]
+    public int Id { get; set; }
     public int Ra { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
