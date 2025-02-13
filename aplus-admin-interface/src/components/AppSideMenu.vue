ee4875b3cfc614de131660d77cb1284e09eb676a
André Pacini <andrepacini1@hotmail.com>
2025-02-13 18:54:41 -0300

    [front] barra de menu lateral

---
+++ aplus-admin-interface/src/components/AppSideMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="menuItems">
+    <v-btn class="menuBtn" variant="plain"  @click="handleHomeRoute">Home</v-btn>
+    <v-btn class="menuBtn" variant="plain" @click="handleStudentRoute">Alunos</v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import router from "@/router";
+
+function handleStudentRoute() {
+  router.push("/alunos");
+}
+
+function handleHomeRoute() {
+    router.push("/")
+}
+</script>
+
+<style lang="scss">
+.menuItems {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+    padding: 1em;
+
+    height: 100%;
+    width: 50%;
+
+    border-radius: 0.7rem;
+
+    background: linear-gradient(180deg, rgba(1,195,203,255) 0%, rgba(1,176,182,255) 50%, rgba(2,156,162,255));
+}
+</style>
