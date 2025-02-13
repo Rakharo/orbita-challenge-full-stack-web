539b1993be52992b15a1108c5089c522c03dcf3b
André Pacini <andrepacini1@hotmail.com>
2025-02-13 18:55:01 -0300

    [front] estruturação da aplicação

--- aplus-admin-interface/src/layouts/default.vue
+++ aplus-admin-interface/src/layouts/default.vue
@@ -1,7 +1,18 @@
 <template>
   <AppTopNavBar />
-  <v-main>
-    <router-view />
+  <v-main style="padding: 2em;">
+    <v-container max-width="100%" height="100%">
+      <v-row style="min-height: 100%;">
+        <v-col cols="3">
+          <AppSideMenu />
+        </v-col>
+        <v-col cols="9">
+          <div class="bodyInfo">
+            <router-view />
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-main>
 
   <AppFooter />
@@ -10,3 +21,15 @@
 <script lang="ts" setup>
   //
 </script>
+
+<style lang="scss">
+.bodyInfo {
+  height: 100%;
+
+  background-color: #FaFafa;
+  box-shadow: 5px 5px rgba(0, 0, 0, 0.2);;
+
+  border-radius: 0.7rem;
+
+}
+</style>
