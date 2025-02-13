2cd36b684157e0d015d648d089f551d62b4c31a7
André Pacini <andrepacini1@hotmail.com>
2025-02-12 21:25:15 -0300

    [front] barra superior com logo do Grupo

---
+++ aplus-admin-interface/src/components/AppTopNavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <img 
+    ref="logo"
+    class="logoImg"
+    src="../assets/logo-mais-a-educacao.svg"
+  >
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+    .logoImg {
+        width: 100px;
+        height: 100px;
+
+        padding: 1em;
+    }
+</style>
