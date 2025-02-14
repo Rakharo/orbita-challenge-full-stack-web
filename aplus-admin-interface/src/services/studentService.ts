7e416e3a7f9a52e62b8fb0c160ba4bf31c3ac9b3
André Pacini <andrepacini1@hotmail.com>
2025-02-14 02:22:55 -0300

    [front] novas funções de chamadas de api

--- aplus-admin-interface/src/services/studentService.ts
+++ aplus-admin-interface/src/services/studentService.ts
@@ -1,3 +1,4 @@
+import type { iStudent } from "@/interfaces/studentInterface";
 import { clientApi } from "@/plugins/clientApi";
 
 export async function getAllStudents(pageNumber: number, pageSize: number) {
@@ -5,7 +6,37 @@ export async function getAllStudents(pageNumber: number, pageSize: number) {
         const response = await clientApi.get(`Students?pageNumber=${pageNumber}&pageSize=${pageSize}`)
 
         return response.data;
+    } catch(error) {
+        console.error(error)
+    }
+}
+
+export async function createNewStudent(student: iStudent) {
+    try {
+        const response = await clientApi.post('Students', student)
+
+        return response.data
     } catch(error: unknown) {
+        console.error(error)
+    }
+}
+
+export async function editStudent(ra: string, student: iStudent) {{
+    try {
+        const response = await clientApi.put(`Students/${ra}`, student)
+
+        return response.data;
+    } catch(error) {
+        console.error(error)
+    }
+}}
+
+export async function deleteStudent(ra: string) {
+    try {
+        const response = await clientApi.delete(`Students/${ra}`)
+
+        return response.data
+    } catch(error) {
         console.error(error)
     }
 }
